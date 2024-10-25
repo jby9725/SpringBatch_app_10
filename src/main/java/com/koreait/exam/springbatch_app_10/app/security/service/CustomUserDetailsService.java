eb67f37e344e573338de5dfdb7d238c3e6b3841b
jby9725 <jby9725@gmail.com>
2024-10-25 15:36:17 +0900

    user1을 관리자로 지정, 관리자 페이지 접근 제한

--- src/main/java/com/koreait/exam/springbatch_app_10/app/security/service/CustomUserDetailsService.java
+++ src/main/java/com/koreait/exam/springbatch_app_10/app/security/service/CustomUserDetailsService.java
@@ -15,20 +15,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor // final 변수에 대한 생성자 자동 생성
-public class CustomUserDetailsService implements UserDetailsService { // UserDetailsService : 사용자 인증 정보 조회
-
-    // 데이터의 출처는 DB -> 불러와서 Spring Security에서 사용 가능하도록 변환
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService { // 사용자 인증 정보 조회
+    // 데이터의 출처는 DB -> Spring Security에서 사용가능하게 변환
     private final MemberRepository memberRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-
         Member member = memberRepository.findByUsername(username).get();
-
         List<GrantedAuthority> authorities = new ArrayList<>();
-        authorities.add(new SimpleGrantedAuthority("MEMBER")); // MEMBER 권한을 부여. 권한 객체는 SimpleGrantedAuthority
 
-        return new MemberContext(member, authorities); // 멤버와 권한을 합치기
+        if (member.getUsername().equals("user1")) {
+            authorities.add(new SimpleGrantedAuthority("ADMIN"));
+        }
+
+        authorities.add(new SimpleGrantedAuthority("MEMBER")); // MEMBER 권한을 부여/ 권한 객체는 SimpleGrantedAuthority
+        return new MemberContext(member, authorities);
     }
 }
